c580c62536c980eeade9334cf2bc0f14cbceb5a6
Bart Hoekstra <bart-hoekstra@live.nl>
2015-05-19 16:19:17 +0200

    Merge fix

--- db/schema.rb
+++ db/schema.rb
@@ -11,8 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-ActiveRecord::Schema.define(version: 20150519100159) do
+ActiveRecord::Schema.define(version: 20150519113800) do
 
   create_table "categories", force: :cascade do |t|
     t.string "title"
