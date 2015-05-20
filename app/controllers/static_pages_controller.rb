6b48166daeb0155947a436f2c9398726323c3f82
H-Renkema <hgr93@planet.nl>
2015-05-20 11:42:08 +0200

    add categories

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -2,4 +2,15 @@ class StaticPagesController < ApplicationController
   def home
   	@question = Question.order('RANDOM()').first
   end
+
+  def pick_category
+  	@reading = Question.where(category: 1).order('RANDOM()').first
+  	@writing = Question.where(category: 2).order('RANDOM()').first
+  end
+
+  def pick_language
+  	@dutch = Question.where(language: 3).order('RANDOM()').first
+  	@english = Question.where(language: 2).order('RANDOM()').first
+  	@frisian = Question.where(language: 1).order('RANDOM()').first
+  end
 end
