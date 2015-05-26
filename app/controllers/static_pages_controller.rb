d79021926d522e51e33147e9442438d49918cc6a
Bart Hoekstra <bart-hoekstra@live.nl>
2015-05-26 10:19:52 +0200

    Added base styling

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,4 +1,7 @@
 class StaticPagesController < ApplicationController
+  
+  before_action :set_user_results, only: [:home]
+
   def home
   	@question = Question.where(approved: true).order('RANDOM()').first
   end
@@ -13,4 +16,13 @@ class StaticPagesController < ApplicationController
   	@english = Question.where(language: 2, approved: true).order('RANDOM()').first
   	@frisian = Question.where(language: 1, approved: true).order('RANDOM()').first
   end
+
+  private
+
+    def set_user_results
+      fries_id = Language.where(title: 'Fries').first.id
+      @fries_user_results_true = current_user.user_results.where(language_id: fries_id, correctly_answered: true).count
+      @fries_user_results_false = current_user.user_results.where(language_id: fries_id, correctly_answered: false).count
+    end
+
 end
--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -34,6 +34,10 @@ class ImageUploader < CarrierWave::Uploader::Base
      process :resize_to_fit => [200, 200]
   end
 
+  version :mini_thumb do
+     process :resize_to_fit => [150, 150]
+  end
+
   # Add a white list of extensions which are allowed to be uploaded.
   # For images you might use something like this:
   def extension_white_list
