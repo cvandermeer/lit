27ba394871710730746a9aef0e79b5527945b6e6
Chris van der Meer <cvandermeer@live.nl>
2015-05-28 12:51:56 +0200

    fixed random function

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -5,14 +5,14 @@ class StaticPagesController < ApplicationController
   end
 
   def pick_category
-  	@reading = Question.where(category_id: 1, approved: true).offset(rand(Question.count)).first
-    @foto_quiz = Question.where(category_id: 2, approved: true).offset(rand(Question.count)).first
-  	@writing = Question.where(category_id: 3, approved: true).offset(rand(Question.count)).first
+  	@reading = Question.where(category_id: 1, approved: true).offset(rand(Question.where(category_id: 1, approved: true).count)).first
+    @foto_quiz = Question.where(category_id: 2, approved: true).offset(rand(Question.where(category_id: 2, approved: true).count)).first
+  	@writing = Question.where(category_id: 3, approved: true).offset(rand(Question.where(category_id: 3, approved: true).count)).first
   end
 
   def pick_language
-  	@dutch = Question.where(language: 3, approved: true).offset(rand(Question.count)).first
-  	@english = Question.where(language: 2, approved: true).offset(rand(Question.count)).first
-  	@frisian = Question.where(language: 1, approved: true).offset(rand(Question.count)).first
+  	@dutch = Question.where(language: 3, approved: true).offset(rand(Question.where(language: 3, approved: true).count)).first
+  	@english = Question.where(language: 2, approved: true).offset(rand(Question.where(language: 2, approved: true).count)).first
+  	@frisian = Question.where(language: 1, approved: true).offset(rand(Question.where(language: 1, approved: true).count)).first
   end
 end
