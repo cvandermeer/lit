19476c06a3afbf548d0a6d92a4f476aecd40b702
Chris van der Meer <cvandermeer@live.nl>
2015-06-16 13:52:00 +0200

    fixing dependend destroy

--- app/models/team.rb
+++ app/models/team.rb
@@ -6,7 +6,7 @@ class Team < ActiveRecord::Base
 	### RELATIONS ###
 	has_many :memberships
 	has_many :users, through: :memberships
-	belongs_to :user
+	belongs_to :user, dependent: :destroy
 
 	### VALIDATIONS ###
 	validates :name, presence: true
