132618a841d6ce156fbbc88ce3300227cc6055e7
Bart Hoekstra <bart-hoekstra@live.nl>
2015-06-25 13:54:01 +0200

    First view tests

---
+++ test/controllers/users_controller_test.rb
@@ -0,0 +1,39 @@
+require 'test_helper'
+
+class UsersControllerTest < ActionController::TestCase
+  setup :login_user, :set_user
+ 
+  test 'should get show' do
+    get :show, id: @user.id
+    assert_response :success
+  end
+
+  test 'should get leaderboard' do
+    get :leaderboard
+    assert_response :success
+  end
+
+  test 'should get leaderboard choose image' do
+    get :leaderboard_choose_image
+    assert_response :success
+  end
+
+  test 'should get leaderboard normal questions' do
+    get :leaderboard_normal_questions
+    assert_response :success
+  end
+
+  test 'should get leaderboard write word' do
+    get :leaderboard_write_word
+    assert_response :success
+  end
+
+  private
+    def login_user 
+      sign_in users(:one)
+    end
+
+    def set_user
+      @user = users(:one)
+    end
+end
---
+++ test/controllers/word_of_the_weeks_controller_test.rb
@@ -0,0 +1,31 @@
+require 'test_helper'
+
+class WordOfTheWeeksControllerTest < ActionController::TestCase
+  setup :login_user, :set_word_of_the_week
+ 
+
+  test 'should get index' do
+    get :index
+    assert_response :success
+  end
+
+  test 'should get show' do
+    get :show, id: @word_of_the_week.id
+    assert_response :success
+  end
+
+  test 'should get edit' do
+    get :edit, id: @word_of_the_week.id
+    assert_response :success
+  end
+
+  private
+    def login_user 
+      sign_in users(:one)
+    end 
+
+    def set_word_of_the_week
+      @word_of_the_week = word_of_the_weeks(:word_of_the_week)
+    end
+
+end
