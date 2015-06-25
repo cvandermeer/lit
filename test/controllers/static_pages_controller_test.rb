e89589ccafecebe94a33d6d7fb3247087b7d2c05
Bart Hoekstra <bart-hoekstra@live.nl>
2015-06-25 12:44:09 +0200

    Static pages controller view test

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -8,6 +8,21 @@ class StaticPagesControllerTest < ActionController::TestCase
     assert_response :success
   end
 
+  test 'should get choose game' do
+  	get :choose_game
+  	assert_response :success
+  end
+
+  test 'should get pick category' do
+  	get :pick_category
+  	assert_response :success
+  end
+
+  test 'should get pick language' do
+  	get :pick_language
+  	assert_response :success
+  end
+
   private
     def login_user
       sign_in users(:one)
